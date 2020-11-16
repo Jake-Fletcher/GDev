7021a8f89eb3f75e2844ca460a9aec1299821963
Jake Fletcher <20080415@mail.wit.ie>
2020-11-16 03:12:27 +0000

    shooty

---
+++ Assets/CollisionWithProjectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionWithProjectile : MonoBehaviour
+{
+    public GameObject explosion;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            SceneManager.LoadScene("lvl-2");
+        }
+    }
+}
---
+++ Assets/LaunchProjectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchProjectile : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public GameObject target;
+    float time;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.F))
+        time += Time.deltaTime;
+        if (time > 1.0f)
+        {
+            time = 0;
+            GameObject t = (GameObject)Instantiate(projectile, transform.position, Quaternion.identity);
+            Destroy(t, 3);
+            transform.LookAt(target.transform);
+            t.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+        }
+
+    }
+}
