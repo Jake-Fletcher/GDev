051930d3a298ba6721b34665241b8d4b1f215887
Jake Fletcher <20080415@mail.wit.ie>
2020-11-16 03:49:40 +0000

    npc stuff

---
+++ Assets/ControlNPC.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlNPC : MonoBehaviour
+{
+    Animator anim;
+    AnimatorStateInfo info;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        info = anim.GetCurrentAnimatorStateInfo(0);
+       if(Input.GetKeyDown(KeyCode.I))
+        {
+            anim.SetBool("CanSeePlayer", true);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            anim.SetBool("CanSeePlayer", false);
+        }
+
+        if (info.IsName("Idle"))
+        {
+            Debug.Log("In Idle");
+        }
+        
+        else if (info.IsName("FollowPlayer"))
+        {
+            Debug.Log("Follow Player state");
+        }
+
+
+    }
+}
